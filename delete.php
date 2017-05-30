a33a7a7099128b8175e01a60ed9ea2c57326e08b
Xavier POUZENC <xpouzenc@e1r4p22.42.fr>
2017-05-30 16:48:17 +0200

    working on the delete story images

---
+++ delete.php
@@ -0,0 +1,23 @@
+<?php 
+
+	session_start();
+	if (isset($_SESSION['user']) && isset($_GET['id']) && !empty($_GET['id'])){
+		require_once('includes/bootstrap.php');
+		
+		$conn = App::getDatabase();
+		$img = $conn->query('SELECT * FROM images WHERE id = ?', [$_GET['id']])->fetch();
+
+		if ($img)
+		{
+			$conn->query('DELETE FROM images WHERE id = ?', [$_GET['id']]);
+			header('Location: story.php');
+		}
+		else {
+			header('Location: 404.php');
+		}
+	}
+	else {
+		header('Location: 404.php');
+	}
+
+?>
--- story.php
+++ story.php
@@ -1,6 +1,7 @@
 <?php
 
 	session_start();
+	require_once('includes/bootstrap.php');
 
 ?>
 <?php if (isset($_SESSION['user'])): require('themes/header.php'); ?>
@@ -33,6 +34,63 @@
 
 	<input id="file" type="file" multiple />
 	
+	<div class="separator"></div>
+
+	<div class="gallery">
+
+		<?php
+
+			$conn = App::getDatabase();
+			$pics = $conn->query('SELECT * FROM images WHERE username = ? ORDER BY id desc', [$_SESSION['user']])->fetchAll();
+
+			if ($pics) {
+				foreach ($pics as $key => $value) { ?>
+					<div class="card align-left">
+					<img src="db_image/<?php echo $value['name']; ?>.png" alt="<?php echo $value['name']; ?>">
+					<div class="underbar">
+						<span class="user">
+							<span class="date"><?php echo date('F j, Y - H:i A', strtotime($value['created']));; ?></span>
+						</span>
+						<?php if (isset($_SESSION['user'])): ?>
+							<ul>
+								<li><a href="image.php?id=<?php echo $value['id']; ?>" title="Comment it"><i class="fa fa-comment" aria-hidden="true"></i>
+								<?php
+
+									$com = $conn->query('SELECT COUNT(*) FROM comments WHERE id_image = ?', [$value['id']])->fetchColumn();
+									if ($com) { echo '<span class="count">'.$com.'</span>'; } else { echo ""; }
+
+								?>
+								</a>
+								</li>
+								<li><a href="like.php?id=<?php echo $value['id']; ?>" title="Like it"><i class="fa fa-heart
+								<?php
+
+									$find = $conn->query('SELECT * FROM likes WHERE id_user = ? AND id_image = ?', [$_SESSION['user_id'],$value['id']])->fetch();
+									if ($find) { echo "blue"; } else { echo ""; }
+
+								?>" aria-hidden="true"></i>
+								<?php
+
+									$id = $conn->query('SELECT COUNT(*) FROM likes WHERE id_image = ?', [$value['id']])->fetchColumn();
+									if ($id) { echo '<span class="count">'.$id.'</span>'; } else { echo ""; }
+
+								?>
+								</a>
+								</li>
+								<li><a href="delete.php?id=<?php echo $value['id']; ?>" title="Delete it"><i class="fa fa-trash red" aria-hidden="true"></i></a></li>
+							</ul>
+						<?php endif; ?>
+					</div>
+				</div>
+				<?php
+				}
+			}
+
+		?>
+
+				<div class="clear"></div>
+		
+	</div>
 
 	<script src="themes/js/func.js"></script>
 	<script src="themes/js/camagru.js"></script>
