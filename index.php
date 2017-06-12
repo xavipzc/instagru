a870786b0a0c361c2661e7a97d40aabcb5a300f7
Xavier POUZENC <xpouzenc@e1r11p15.42.fr>
2017-06-12 12:46:55 +0200

    database check in the index.php file

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
 	
-	session_start();
+	require('config/database.php');
+	$pdo = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+	$req = $pdo->prepare('SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = ?');
+	$req->execute(array('camagru'));
 
-	if ($_SESSION['user'])
-	{
-		header("Location: timeline.php");
+	if ($req->fetchColumn() == 0) {
+
+		echo "The database doesn't exist, please launch the config/setup.php file";
+
 	}
-	else
-	{
-		header("Location: login.php");
+
+	else {
+
+		session_start();
+
+		if ($_SESSION['user'])
+		{
+			header("Location: timeline.php");
+		}
+		else
+		{
+			header("Location: login.php");
+		}
+
 	}
 
 ?>
